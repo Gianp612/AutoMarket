6792e17c47226af57028e185c071d3208765e40c
Gian-612 <bohorquezlandeogiancarlos@gmail.com>
2026-09-12 11:30:09 -0500

    Create Vehiculo.js

---
+++ Automarketbackend/src/models/Vehiculo.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const vehiculoSchema = new mongoose.Schema({
+  marca: { type: String, required: true },
+  modelo: { type: String, required: true },
+  anio: { type: Number, required: true },
+  precio: { type: Number, required: true },
+  caracteristicas: { type: String },
+  descripcion: { type: String },
+  icono: { type: String },
+});
+
+module.exports = mongoose.model("Vehiculo", vehiculoSchema);
