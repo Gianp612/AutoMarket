927173156e7a7b3b9c7726e53df34bc4e39e32fb
Gian-612 <bohorquezlandeogiancarlos@gmail.com>
2026-09-12 11:47:30 -0500

    Create usuarios.routes.js

---
+++ Automarketbackend/src/routes/usuarios.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const Usuario = require("../models/Usuario");
+
+router.get("/", async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const nuevoUsuario = await Usuario.create(req.body);
+    res.status(201).json(nuevoUsuario);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
